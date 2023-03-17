b6d3b05d85128b0df618df9959b1b67f0192641a
shrek wang <shrekwang1@163.com>
2023-03-17 22:30:29 +0800

    Paginated reader: pass the correct page token from the pagination storage to the buffered reader.

--- paginated_reader/chat_record_paginated_reader_test.go
+++ paginated_reader/chat_record_paginated_reader_test.go
@@ -20,3 +20,48 @@ func TestFetchPageToken_zero(t *testing.T) {
 	// When
 	paginatedReader.Read()
 }
+
+func TestFetchPageToken_one(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	bufferedReader := NewMockChatRecordPaginatedBufferedReader(ctrl)
+	paginationStorage := NewMockChatRecordPaginationStorage(ctrl)
+	paginatedReader := NewChatRecordPaginatedReader(bufferedReader, paginationStorage)
+
+	// Then
+	paginationStorage.EXPECT().Get().Return(PageToken(1), nil).Times(1)
+	bufferedReader.EXPECT().Read(gomock.Eq(PageToken(1))).Return([]*business.ChatRecord{}, nil).Times(1)
+
+	// When
+	paginatedReader.Read()
+}
+
+func TestFetchPageToken_many(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	bufferedReader := NewMockChatRecordPaginatedBufferedReader(ctrl)
+	paginationStorage := NewMockChatRecordPaginationStorage(ctrl)
+	paginatedReader := NewChatRecordPaginatedReader(bufferedReader, paginationStorage)
+
+	// Then
+	paginationStorage.EXPECT().Get().Return(PageToken(3), nil).Times(1)
+	bufferedReader.EXPECT().Read(gomock.Eq(PageToken(3))).Return([]*business.ChatRecord{}, nil).Times(1)
+
+	// When
+	paginatedReader.Read()
+}
+
+func TestFetchPageToken_ALot(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	bufferedReader := NewMockChatRecordPaginatedBufferedReader(ctrl)
+	paginationStorage := NewMockChatRecordPaginationStorage(ctrl)
+	paginatedReader := NewChatRecordPaginatedReader(bufferedReader, paginationStorage)
+
+	// Then
+	paginationStorage.EXPECT().Get().Return(PageToken(10500), nil).Times(1)
+	bufferedReader.EXPECT().Read(gomock.Eq(PageToken(10500))).Return([]*business.ChatRecord{}, nil).Times(1)
+
+	// When
+	paginatedReader.Read()
+}
