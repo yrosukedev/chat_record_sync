bb4bd41e2e855e3a962fc08f0b01c6b2a2a11ddf
shrek wang <shrekwang1@163.com>
2023-03-18 17:28:46 +0800

    Buffer reader: forward the result when the buffer size is greater than 1.

--- buffer_reader/chat_record_buffered_reader_adapter.go
+++ buffer_reader/chat_record_buffered_reader_adapter.go
@@ -2,24 +2,49 @@ package buffer_reader
 
 import (
 	"github.com/yrosukedev/chat_record_sync/business"
+	"io"
 )
 
 type ChatRecordBufferedReaderAdapter struct {
-	bufferedReader ChatRecordBufferedReader
+	bufferedReader  ChatRecordBufferedReader
+	idx             int
+	bufferedRecords []*business.ChatRecord
 }
 
 func NewChatRecordBufferedReaderAdapter(bufferedReader ChatRecordBufferedReader) *ChatRecordBufferedReaderAdapter {
 	return &ChatRecordBufferedReaderAdapter{
 		bufferedReader: bufferedReader,
+		idx:            0,
 	}
 }
 
 func (c *ChatRecordBufferedReaderAdapter) Read() (record *business.ChatRecord, err error) {
-	records, err := c.bufferedReader.Read()
-
-	if len(records) == 0 {
+	if err := c.refillBufferIfNeeded(); err != nil {
 		return nil, err
 	}
 
-	return records[0], err
+	return c.readFromBuffer()
+}
+
+func (c *ChatRecordBufferedReaderAdapter) refillBufferIfNeeded() error {
+	if !c.needRefill() {
+		return nil
+	}
+
+	var err error
+	c.bufferedRecords, err = c.bufferedReader.Read()
+	return err
+}
+
+func (c *ChatRecordBufferedReaderAdapter) needRefill() bool {
+	return c.idx >= len(c.bufferedRecords)
+}
+
+func (c *ChatRecordBufferedReaderAdapter) readFromBuffer() (*business.ChatRecord, error) {
+	if c.needRefill() { // still need refill, but already refilled, that means reaching the end
+		return nil, io.EOF
+	}
+
+	defer func() { c.idx += 1 }()
+	return c.bufferedRecords[c.idx], nil
 }
--- buffer_reader/chat_record_buffered_reader_adapter_test.go
+++ buffer_reader/chat_record_buffered_reader_adapter_test.go
@@ -39,3 +39,40 @@ func TestBufferSize_zero(t *testing.T) {
 		_, _ = readerAdapter.Read()
 	}
 }
+
+func TestBufferSize_greaterThanOne(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	bufferedReader := NewMockChatRecordBufferedReader(ctrl)
+	readerAdapter := NewChatRecordBufferedReaderAdapter(bufferedReader)
+
+	// Then
+	records := []*business.ChatRecord{
+		{
+			MsgId: "1",
+		},
+		{
+			MsgId: "2",
+		},
+		{
+			MsgId: "3",
+		},
+	}
+	bufferedReader.EXPECT().Read().Return(records, nil).Times(1)
+
+	// When
+	expectReaderToReadRecords(t, readerAdapter, records)
+}
+
+func expectReaderToReadRecords(t *testing.T, reader *ChatRecordBufferedReaderAdapter, records []*business.ChatRecord) {
+	for _, expected := range records {
+		actual, err := reader.Read()
+		if err != nil {
+			t.Errorf("error should not happen here, expected: %+v, actual: %+v", nil, err)
+		}
+
+		if expected != actual {
+			t.Errorf("records are not matched, expected: %+v, actual: %+v", expected, actual)
+		}
+	}
+}
