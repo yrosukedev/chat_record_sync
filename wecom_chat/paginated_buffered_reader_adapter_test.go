0b0558d2f51cb2b8ec5db56724470b560750f1e3
shrek wang <shrekwang1@163.com>
2023-03-21 18:07:29 +0800

    WeCom chat: don't call open API to get the contacts if there is no contact.

--- wecom_chat/paginated_buffered_reader_adapter_test.go
+++ wecom_chat/paginated_buffered_reader_adapter_test.go
@@ -99,3 +99,52 @@ func TestOneRecord_oneReceiver(t *testing.T) {
 		return
 	}
 }
+
+func TestOneRecord_zeroReceiver(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	chatRecordService := NewMockChatRecordService(ctrl)
+	openAPIService := NewMockOpenAPIService(ctrl)
+	transformer := NewMockChatRecordTransformer(ctrl)
+	readerAdapter := NewPaginatedBufferedReaderAdapter(chatRecordService, openAPIService, transformer)
+
+	// Then
+	wecomRecords := []*WeComChatRecord{
+		{
+			Seq:  375,
+			From: "ID_xiaoming",
+		},
+	}
+	user := &WeComUserInfo{
+		UserID: "ID_xiaoming",
+		Name:   "Xiao Ming",
+	}
+	expectedRecords := []*business.ChatRecord{
+		{
+			From: &business.User{
+				UserId: "ID_xiaoming",
+				Name:   "Xiao Ming",
+			},
+		},
+	}
+
+	chatRecordService.EXPECT().Read(gomock.Eq(uint64(267)), gomock.Eq(uint64(10))).Return(wecomRecords, nil).Times(1)
+	openAPIService.EXPECT().GetUserInfoByID(gomock.Eq("ID_xiaoming")).Return(user, nil).Times(1)
+	openAPIService.EXPECT().GetExternalContactByID(gomock.Any()).Times(0)
+	transformer.EXPECT().Transform(gomock.Eq(wecomRecords[0]), gomock.Eq(user), gomock.Nil()).Return(expectedRecords[0], nil).Times(1)
+
+	// When
+	records, outPageToken, err := readerAdapter.Read(paginated_reader.NewPageToken(267), 10)
+	if err != nil {
+		t.Errorf("error shouldn't happen here, expected: %v, actual: %v", nil, err)
+		return
+	}
+	if !reflect.DeepEqual(expectedRecords, records) {
+		t.Errorf("records not matched, expected: %+v, actual: %+v", expectedRecords, records)
+		return
+	}
+	if !reflect.DeepEqual(outPageToken, paginated_reader.NewPageToken(375)) {
+		t.Errorf("output page token not matched, expected: %+v, actual: %+v", paginated_reader.NewPageToken(375), outPageToken)
+		return
+	}
+}
