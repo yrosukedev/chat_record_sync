795600db74a60171bf8a0e8484fa9a8b4024c9b9
shrek wang <shrekwang1@163.com>
2023-04-20 17:06:23 +0800

    Sender Field Transformer: don't change the inputs.

--- chat_sync/wecom/transformer/sender_field_transformer_test.go
+++ chat_sync/wecom/transformer/sender_field_transformer_test.go
@@ -51,3 +51,41 @@ func TestSenderFieldTransformer_Transform_wecomRecordCantBeNil(t *testing.T) {
 		assert.Nil(t, chatRecord)
 	}
 }
+
+func TestSenderFieldTransformer_Transform_dontChangeInputs(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	openAPIService := NewMockOpenAPIService(ctrl)
+	transformer := NewSenderFieldTransformer(openAPIService)
+	wecomRecord := &wecom.ChatRecord{
+		From: "123",
+	}
+	chatRecord := &business.ChatRecord{
+		From: &business.User{
+			UserId: "::whatever user id::",
+			Name:   "::whatever user name::",
+		},
+	}
+	expectedChatRecord := &business.ChatRecord{
+		From: &business.User{
+			UserId: "123",
+			Name:   "haary",
+		},
+	}
+
+	openAPIService.EXPECT().GetUserInfoByID(gomock.Eq("123")).Return(&wecom.UserInfo{
+		UserID: "123",
+		Name:   "haary",
+	}, nil).Times(1)
+
+	// When
+	updatedChatRecord, err := transformer.Transform(wecomRecord, chatRecord)
+
+	// Then
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChatRecord, updatedChatRecord)
+
+		assert.Equal(t, chatRecord.From.UserId, "::whatever user id::")
+		assert.Equal(t, chatRecord.From.Name, "::whatever user name::")
+	}
+}
