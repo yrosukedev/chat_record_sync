2241428c9bb9bbcfdc5fc4d4107bc0573a1fa038
shrek wang <shrekwang1@163.com>
2023-04-20 20:10:07 +0800

    Receiver Field Transformer: transform as many receivers as possible.

--- chat_sync/wecom/transformer/receiver_field_transformer_test.go
+++ chat_sync/wecom/transformer/receiver_field_transformer_test.go
@@ -105,3 +105,50 @@ func TestReceiverFieldTransformer_Transform_zeroReceiver(t *testing.T) {
 		assert.Equal(t, expectedChatRecord, chatRecord)
 	}
 }
+
+func TestReceiverFieldTransformer_Transform_manyReceivers(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	openAPIService := NewMockOpenAPIService(ctrl)
+	transformer := NewReceiverFieldTransformer(openAPIService)
+	wecomRecord := &wecom.ChatRecord{
+		ToList: []string{"123", "456", "789"},
+	}
+	expectedChatRecord := &business.ChatRecord{
+		To: []*business.User{
+			{
+				UserId: "123",
+				Name:   "haary",
+			},
+			{
+				UserId: "456",
+				Name:   "lucy",
+			},
+			{
+				UserId: "789",
+				Name:   "lily",
+			},
+		},
+	}
+
+	openAPIService.EXPECT().GetExternalContactByID(gomock.Eq("123")).Return(&wecom.ExternalContact{
+		ExternalUserID: "123",
+		Name:           "haary",
+	}, nil).Times(1)
+	openAPIService.EXPECT().GetExternalContactByID(gomock.Eq("456")).Return(&wecom.ExternalContact{
+		ExternalUserID: "456",
+		Name:           "lucy",
+	}, nil).Times(1)
+	openAPIService.EXPECT().GetExternalContactByID(gomock.Eq("789")).Return(&wecom.ExternalContact{
+		ExternalUserID: "789",
+		Name:           "lily",
+	}, nil).Times(1)
+
+	// When
+	chatRecord, err := transformer.Transform(wecomRecord, nil)
+
+	// Then
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChatRecord, chatRecord)
+	}
+}
