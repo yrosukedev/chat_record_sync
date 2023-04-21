414366f54aa640cfb7ccfac20b3bbd9658327ac2
shrek wang <shrekwang1@163.com>
2023-04-21 18:11:13 +0800

    Other Content Field Transformer: if the input chat record is nil, create one for the output.

---
+++ chat_sync/wecom/transformer/other_content_field_transformer_test.go
@@ -0,0 +1,29 @@
+package transformer
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/yrosukedev/chat_record_sync/chat_sync/business"
+	"github.com/yrosukedev/chat_record_sync/chat_sync/wecom"
+	"testing"
+)
+
+func TestOtherContentFieldTransformer_Transform_nilChatRecord(t *testing.T) {
+	// Given
+	transformer := NewOtherContentFieldTransformer()
+	originMessage := "{\"msgid\":\"2641513858500683770_1603876152\",\"action\":\"send\",\"from\":\"icefog\",\"tolist\":[\"wmN6etBgAA0sbJ3invMvRxPQDFoq9uWA\"],\"roomid\":\"\",\"msgtime\":1603876152141,\"msgtype\":\"location\",\"location\":{\"longitude\":116.586285899,\"latitude\":39.911125799,\"address\":\"北京市xxx区xxx路xxx大厦x座\",\"title\":\"xxx管理中心\",\"zoom\":15}}"
+	wecomRecord := &wecom.ChatRecord{
+		MsgType:       "location",
+		OriginMessage: []byte(originMessage),
+	}
+	expectedChatRecord := &business.ChatRecord{
+		Content: originMessage,
+	}
+
+	// When
+	chatRecord, err := transformer.Transform(wecomRecord, nil)
+
+	// Then
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChatRecord, chatRecord)
+	}
+}
