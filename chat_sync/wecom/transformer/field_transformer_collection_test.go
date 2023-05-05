9b854834bd2d89f14750d30c917362eb065ff38c
shrek wang <shrekwang1@163.com>
2023-05-05 11:29:47 +0800

    Field Transformer Collection: one transformer.

--- chat_sync/wecom/transformer/field_transformer_collection_test.go
+++ chat_sync/wecom/transformer/field_transformer_collection_test.go
@@ -1,6 +1,7 @@
 package transformer
 
 import (
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/yrosukedev/chat_record_sync/chat_sync/business"
 	"github.com/yrosukedev/chat_record_sync/chat_sync/wecom"
@@ -12,6 +13,25 @@ func TestFieldTransformerCollection_Transform_ZeroTransformer(t *testing.T) {
 	collection := NewFieldTransformerCollection(nil)
 	wecomRecord := &wecom.ChatRecord{}
 	var expectedChatRecord *business.ChatRecord
+
+	// When
+	chatRecord, err := collection.Transform(wecomRecord, nil)
+
+	// Then
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChatRecord, chatRecord)
+	}
+}
+
+func TestFieldTransformerCollection_Transform_OneTransformer(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	transformer := NewMockFieldTransformer(ctrl)
+	collection := NewFieldTransformerCollection([]FieldTransformer{transformer})
+	wecomRecord := &wecom.ChatRecord{}
+	expectedChatRecord := &business.ChatRecord{}
+
+	transformer.EXPECT().Transform(gomock.Eq(wecomRecord), gomock.Nil()).Return(expectedChatRecord, nil).Times(1)
 
 	// When
 	chatRecord, err := collection.Transform(wecomRecord, nil)
