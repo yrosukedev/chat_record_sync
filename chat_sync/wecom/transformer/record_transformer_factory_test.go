08187b5dfc8fa4dfb6ad1691f637d683aab16a2d
shrek wang <shrekwang1@163.com>
2023-05-05 17:02:41 +0800

    Record Transformer Factory: use the transformer found in the mapping.

--- chat_sync/wecom/transformer/record_transformer_factory_test.go
+++ chat_sync/wecom/transformer/record_transformer_factory_test.go
@@ -43,3 +43,41 @@ func TestRecordTransformerFactory_Transform_TransformerNotFoundForMessageType(t 
 		assert.Equal(t, expectedChatRecord, chatRecord)
 	}
 }
+
+func TestRecordTransformerFactory_Transform_TransformerFoundForMessageType(t *testing.T) {
+	// if the transformer for the message type is found, the transformer for the message type should be used.
+	// both the default transformer and the transformer for the message type should be mocked.
+
+	// Given
+	ctrl := gomock.NewController(t)
+	defaultTransformer := NewMockFieldTransformer(ctrl)
+	fieldATransformer := NewMockFieldTransformer(ctrl)
+	fieldBTransformer := NewMockFieldTransformer(ctrl)
+	fieldXTransformer := NewMockFieldTransformer(ctrl)
+	messageType := "message type x"
+	messageTypeToTransformer := map[string]FieldTransformer{
+		fmt.Sprintf("::message type not %v::", messageType):         fieldATransformer,
+		fmt.Sprintf("::message type not %v neither::", messageType): fieldBTransformer,
+		messageType: fieldXTransformer,
+	}
+	factory := NewRecordTransformerFactory(messageTypeToTransformer, defaultTransformer)
+	wecomRecord := &wecom.ChatRecord{
+		MsgType: messageType,
+	}
+	expectedChatRecord := &business.ChatRecord{
+		MsgType: messageType,
+	}
+
+	defaultTransformer.EXPECT().Transform(gomock.Any(), gomock.Any()).Return(expectedChatRecord, nil).Times(0)
+	fieldATransformer.EXPECT().Transform(gomock.Any(), gomock.Any()).Return(expectedChatRecord, nil).Times(0)
+	fieldBTransformer.EXPECT().Transform(gomock.Any(), gomock.Any()).Return(expectedChatRecord, nil).Times(0)
+	fieldXTransformer.EXPECT().Transform(gomock.Eq(wecomRecord), gomock.Nil()).Return(expectedChatRecord, nil).Times(1)
+
+	// When
+	chatRecord, err := factory.Transform(wecomRecord)
+
+	// Then
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChatRecord, chatRecord)
+	}
+}
