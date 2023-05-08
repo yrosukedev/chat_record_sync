91e5c39e964554e267729069c574da55bf6a1b52
shrek wang <shrekwang1@163.com>
2023-05-08 13:01:36 +0800

    Room Field Transformer: if the input chat record is nil, create one.

---
+++ chat_sync/wecom/transformer/room_field_transformer.go
@@ -0,0 +1,45 @@
+package transformer
+
+import (
+	"github.com/yrosukedev/chat_record_sync/chat_sync/business"
+	"github.com/yrosukedev/chat_record_sync/chat_sync/wecom"
+)
+
+type RoomFieldTransformer struct {
+	nameFetcher NameFetcher
+}
+
+func NewRoomFieldTransformer(nameFetcher NameFetcher) *RoomFieldTransformer {
+	return &RoomFieldTransformer{
+		nameFetcher: nameFetcher,
+	}
+}
+
+func (t *RoomFieldTransformer) Transform(wecomRecord *wecom.ChatRecord, chatRecord *business.ChatRecord) (updatedChatRecord *business.ChatRecord, err error) {
+	updatedChatRecord = t.copyInputIfNeeded(chatRecord)
+
+	name, err := t.nameFetcher.FetchName(wecomRecord.RoomID)
+
+	// fatal tolerated
+	// logging is done in openAPIService
+	if err != nil {
+		updatedChatRecord.Room = &business.Room{
+			RoomId: wecomRecord.RoomID,
+		}
+	} else {
+		updatedChatRecord.Room = &business.Room{
+			RoomId: wecomRecord.RoomID,
+			Name:   name,
+		}
+	}
+
+	return updatedChatRecord, nil
+}
+
+func (t *RoomFieldTransformer) copyInputIfNeeded(chatRecord *business.ChatRecord) *business.ChatRecord {
+	updatedChatRecord := &business.ChatRecord{}
+	if chatRecord != nil {
+		*updatedChatRecord = *chatRecord
+	}
+	return updatedChatRecord
+}
---
+++ chat_sync/wecom/transformer/room_field_transformer_test.go
@@ -0,0 +1,36 @@
+package transformer
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/yrosukedev/chat_record_sync/chat_sync/business"
+	"github.com/yrosukedev/chat_record_sync/chat_sync/wecom"
+	"testing"
+)
+
+func TestRoomFieldTransformer_Transform_NilChatRecord(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	nameFetcher := NewMockNameFetcher(ctrl)
+	transformer := NewRoomFieldTransformer(nameFetcher)
+	wecomRecord := &wecom.ChatRecord{
+		RoomID: "123",
+	}
+	expectedChatRecord := &business.ChatRecord{
+		Room: &business.Room{
+			RoomId: "123",
+			Name:   "room123",
+		},
+	}
+
+	nameFetcher.EXPECT().FetchName(gomock.Eq("123")).Return("room123", nil).Times(1)
+
+	// When
+	chatRecord, err := transformer.Transform(wecomRecord, nil)
+
+	// Then
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChatRecord, chatRecord)
+	}
+}
+
