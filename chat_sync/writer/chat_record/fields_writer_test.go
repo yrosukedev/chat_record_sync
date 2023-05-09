d0835f9d642f88d13db257d6d18ba5f860818b9f
shrek wang <shrekwang1@163.com>
2023-05-09 14:59:59 +0800

    Fields Writer: if the fields are nil, the storage shouldn't be called.

--- chat_sync/writer/chat_record/fields_writer_test.go
+++ chat_sync/writer/chat_record/fields_writer_test.go
@@ -28,3 +28,24 @@ func TestFieldsWriter_Write_ZeroField(t *testing.T) {
 	// Then
 	assert.NoError(t, err)
 }
+
+func TestFieldsWriter_Write_NilField(t *testing.T) {
+	// if the fields is nil, the storage shouldn't be called.
+
+	// Given
+	ctrl := gomock.NewController(t)
+	fieldsFormatter := NewMockFieldsFormatter(ctrl)
+	fieldsStorage := NewMockFieldsStorage(ctrl)
+	fieldsWriter := NewFieldsWriter(fieldsFormatter, fieldsStorage)
+
+	chatRecord := &business.ChatRecord{}
+
+	fieldsFormatter.EXPECT().Format(gomock.Eq(chatRecord)).Return(nil, nil).Times(1)
+	fieldsStorage.EXPECT().Write(gomock.Any(), gomock.Any()).Times(0)
+
+	// When
+	err := fieldsWriter.Write(chatRecord, "26df9a5c-55d8-4c52-b6ce-203325568178")
+
+	// Then
+	assert.NoError(t, err)
+}
