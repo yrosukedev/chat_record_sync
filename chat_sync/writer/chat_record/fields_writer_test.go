a8556896d52f0a7a2e1a0155abffb6c91e31852d
shrek wang <shrekwang1@163.com>
2023-05-09 15:08:59 +0800

    Fields Writer: if the size of the fields is greater than 1, the storage should be callled once.

--- chat_sync/writer/chat_record/fields_writer_test.go
+++ chat_sync/writer/chat_record/fields_writer_test.go
@@ -71,7 +71,7 @@ func TestFieldsWriter_Write_NilChatRecord(t *testing.T) {
 }
 
 func TestFieldsWriter_Write_OneField(t *testing.T) {
-	// if the fields is not empty, the storage should be called once.
+	// if the size of the fields is 1, the storage should be called once.
 
 	// Given
 	ctrl := gomock.NewController(t)
@@ -82,6 +82,32 @@ func TestFieldsWriter_Write_OneField(t *testing.T) {
 	chatRecord := &business.ChatRecord{}
 	fields := map[string]interface{}{
 		"field1": "value1",
+	}
+
+	fieldsFormatter.EXPECT().Format(gomock.Eq(chatRecord)).Return(fields, nil).Times(1)
+	fieldsStorage.EXPECT().Write(gomock.Any(), gomock.Any()).Times(1)
+
+	// When
+	err := fieldsWriter.Write(chatRecord, "26df9a5c-55d8-4c52-b6ce-203325568178")
+
+	// Then
+	assert.NoError(t, err)
+}
+
+func TestFieldsWriter_Write_MultipleFields(t *testing.T) {
+	// if the size of the fields is greater than 1, the storage should be called once.
+
+	// Given
+	ctrl := gomock.NewController(t)
+	fieldsFormatter := NewMockFieldsFormatter(ctrl)
+	fieldsStorage := NewMockFieldsStorage(ctrl)
+	fieldsWriter := NewFieldsWriter(fieldsFormatter, fieldsStorage)
+
+	chatRecord := &business.ChatRecord{}
+	fields := map[string]interface{}{
+		"field1": "value1",
+		"field2": "value2",
+		"field3": "value3",
 	}
 
 	fieldsFormatter.EXPECT().Format(gomock.Eq(chatRecord)).Return(fields, nil).Times(1)
