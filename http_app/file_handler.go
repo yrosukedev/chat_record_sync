82c120e2f6349033140263b1103b227eb44bea29
shrek wang <shrekwang1@163.com>
2023-05-10 16:52:20 +0800

    Fix file handler: make sure the file path is relative to current path.

--- http_app/file_handler.go
+++ http_app/file_handler.go
@@ -1,7 +1,10 @@
 package http_app
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (f *HTTPApp) fileHandler(writer http.ResponseWriter, request *http.Request) {
-	http.ServeFile(writer, request, request.URL.Path)
+	http.ServeFile(writer, request, fmt.Sprintf("./%v", request.URL.Path))
 }
